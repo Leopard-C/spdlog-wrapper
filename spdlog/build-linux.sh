08df78b7e9dc375a894ef1b9cf38f59ac89bc314
Leopard-C <leopard.c@outlook.com>
2023-12-13 00:45:56 +0800

    build(lib): update the build script

--- spdlog/build-linux.sh
+++ spdlog/build-linux.sh
@@ -5,41 +5,43 @@ set -e
 PrintUsage()
 {
     echo "usage:"
-    echo "  ./build-linux.sh [toolchain-config]"
+    echo "  ./build-linux.sh [architecture]"
     echo ""
     echo "for example:"
     echo "  ./build-linux.sh"
-    echo "  ./build-linux.sh default"
+    echo "  ./build-linux.sh x86_64"
     echo "  ./build-linux.sh aarch64"
 }
 
 script_abs=$(readlink -f "$0")
 script_dir=$(dirname $script_abs)
 
-toolchain_config=default
+arch=`uname -m`
 
 if [ $# -eq 1 ]; then
-    toolchain_config=$1
+    arch=$1
 elif [ $# -gt 1 ]; then
     PrintUsage
     exit 1
 fi
 
-toolchain_file="$script_dir/cmake/toolchain.$toolchain_config.cmake"
+echo "Architecture: $arch"
+
+toolchain_file="$script_dir/cmake/toolchain.$arch.cmake"
 if [ ! -e "$toolchain_file" ]; then
     echo "ERROR: not found file '$toolchain_file'"
     PrintUsage
     exit 2
 fi
 
-build_dir=$script_dir/build
-out_lib_dir=$script_dir/../spdlog-wrapper/lib/linux
+build_dir=$script_dir/build/$arch
+out_lib_dir=$script_dir/../spdlog-wrapper/lib/linux/$arch
 
-if [ ! -e $build_dir ]; then mkdir $build_dir; fi
+if [ ! -e $build_dir ];   then mkdir -p $build_dir; fi
 if [ ! -e $out_lib_dir ]; then mkdir -p $out_lib_dir; fi
 
-cd build
-cmake .. -DCMAKE_TOOLCHAIN_FILE=$toolchain_file
+cd $build_dir
+cmake ../.. -DCMAKE_TOOLCHAIN_FILE=$toolchain_file
 make -j
 
 cp libspdlog.a $out_lib_dir
